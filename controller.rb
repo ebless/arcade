befe36a4b989091d91957bfdd53c22a5a01e06c1
Ned Bless <ebless@lsoc.org>
2016-05-09 14:49:39 -0500

    Added rps files

---
+++ controller.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require 'sinatra/cookies'
+require_relative 'models/rps.rb'
+
+get '/' do
+
+	erb :index
+end
+
+post '/' do
+	player = Player.new(params[:name])
+	game = Game.new(player, params[:choice])
+	erb :results
+end
